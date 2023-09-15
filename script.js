423b9b7c24ae6c0dba013422eb9105bc6f0a355f
Wolfxela <alexthegreitsas@gmail.com>
2023-09-15 03:14:59 +0300

    made it so the user can add random books that each can be opened with their own data that i'm proud of for now

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const containers = document.querySelectorAll('.shelfSlot')
 const button = document.querySelector('.newBookBtn')
 var i = 0;
-console.log(containers);
+const bookArray = []
+var totalBooks = 0;
+
 button.addEventListener("click", function(){addBook()})
-function addBook()
+
+addBook(1,"fox",1233,"box","the fox in the box")
+addBook(1,"box",1233,"fox","the box in the fox")
+function addBook(inputPages,inputAuthor,inputDate,inputContent,inputTitle)
 {
     
 
     const book = document.createElement("div")
     book.className = "book"
-    book.style.backgroundColor = "rgb" + "(" + randomColorCreator() + "," + randomColorCreator() + "," + randomColorCreator() +")";
+    //make it so the color gets transfered next
+    book.style.backgroundColor = "rgb" + "(" + randomColorCreator() + "," + randomColorCreator() + "," + randomColorCreator() +")"
     containers[i].appendChild(book)
     if(containers[i].children.length === 9)
     {
         i+= 1;
+    }
+    curentBook = new bookMaker(totalBooks,inputPages,inputAuthor,inputDate,inputContent,inputTitle)
+    bookArray[totalBooks] = curentBook;
+    totalBooks++
+    book.addEventListener("click",function(){bookShowCover(bookArray[findSpotInParent(book)])})
+
+    function findSpotInParent(element)
+    {
+      return Array.from(element.parentNode.children).indexOf(element)
     }
     
 
@@ -23,4 +38,43 @@ function randomColorCreator()
 
   return String(Math.floor(Math.random() * 255));
 
+}
+
+function bookMaker(testnr,bookPageCount,bookAuthor,bookDate,bookContent,bookTitle)
+{
+  this.bookPageCount = bookPageCount
+  this.bookAuthor = bookAuthor
+  this.bookDate = bookDate
+  this.bookContent = bookContent
+  this.bookTitle = bookTitle
+}
+
+function bookShowCover(book)
+{
+
+  const bookCover = document.createElement("div")
+  bookCover.className = "bookCover"
+
+  const content = document.createElement("div")
+  content.className = "content"
+  bookCover.appendChild(content);
+
+  const title = document.createElement("div")
+  title.className = "title"
+  title.textContent = book.bookTitle
+  content.appendChild(title);
+
+  const author = document.createElement("div")
+  author.className = "author"
+  author.textContent = book.bookAuthor
+  content.appendChild(author);
+
+  for(var i = 0; i < 4;i++)
+  {
+    const page = document.createElement("div")
+    page.className = "page"
+    bookCover.appendChild(page)
+
+  }
+  document.body.appendChild(bookCover)
 }
