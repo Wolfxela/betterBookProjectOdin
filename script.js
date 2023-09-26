95f4fb7ffd11050e8b6792252ec454d538b3fc17
Wolfxela <alexthegreitsas@gmail.com>
2023-09-26 21:05:05 +0300

    made this ver with classes

--- script.js
+++ script.js
@@ -57,13 +57,15 @@ function randomColorCreator()
   return String(Math.floor(Math.random() * 255));
 
 }
-
-function bookMaker(bookAuthor,bookContent,bookTitle,color)
+class bookMaker
 {
-  this.bookAuthor = bookAuthor
-  this.bookContent = bookContent
-  this.bookTitle = bookTitle
-  this.color = color;
+  constructor(bookAuthor,bookContent,bookTitle,color)
+  {
+    this.bookAuthor = bookAuthor
+    this.bookContent = bookContent
+    this.bookTitle = bookTitle
+    this.color = color;
+  }
 }
 function bookShowCover(book)
 {
@@ -102,17 +104,22 @@ function bookShowOpen(book)
   const openBook = document.createElement("div")
   openBook.className = "openBook";
   openBook.style.backgroundColor = book.color
+
   const openPage1 = document.createElement("div")
   openPage1.classList.add("openPage","left")
   openBook.appendChild(openPage1)
+
   const textZone1 = document.createElement("div")
   textZone1.className = "textZone"
   openPage1.appendChild(textZone1)
+
   const openPage2 = document.createElement("div")
   openPage2.classList.add("openPage","right")
   openBook.appendChild(openPage2)
+
   const textZone2 = document.createElement("div")
   textZone2.className = "textZone"
+  
   if(book.bookContent.length < 300)
   {
     textZone1.textContent = book.bookContent;
